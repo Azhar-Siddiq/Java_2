f2e537fc304f6238eff8d10e78324f227618f267
Azhar-Siddiq <azharsiddiq73@gmail.com>
2023-02-13 08:25:39 +0500

    Java_Assignment_2

---
+++ app.js
@@ -0,0 +1,49 @@
+// var userAge = +prompt("Enter Your Age")
+// if(userAge == 20){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userGender = prompt("Enter Your Gender");
+// if(userGender == "male"){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userAge = +prompt("Enter Your Age");
+// if(userAge === 20){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userAge = +prompt("Enter Your age");
+// if(userAge > 20){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userAge = +prompt("Enter Your Age");
+// if(userAge >= 20){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userAge = +prompt("Enter Your Age");
+// if(userAge >= 18 && userAge <= 40){
+//     console.log("Allow")
+// }else{
+//     console.log("Do Not Allow")
+// }
+
+// var userAge = +prompt("Enter Your Age");
+// var userGender = prompt("Enter Your Gender");
+// if(userAge >= 18 && userAge <=40 && (userGender == "male" || userGender == "female")){
+//     console.log("Allow") ;
+// }else{
+//     console.log("Do Not Allow");
+// }
